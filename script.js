b0313402fcb81c1744c38fa8198e045b878e5fa4
naugtur <naugtur@gmail.com>
2023-02-15 15:18:21 +0100

    more variants

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-window.runThat = function (code) {
+window.runThat1 = function (code) {
+    eval(code);
+};
+window.runThat2 = eval;
+window.runThat3 = function (code, borrowedEval) {
+    borrowedEval(code);
+};
+window.runThat4 = function (code, borrowedEval) {
+    var eval = borrowedEval;
     eval(code);
 };
